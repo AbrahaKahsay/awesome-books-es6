7f9660e7f2e7a460b8a3493171ebbf7184bcbcce
Abraha Kahsay <abrahak81@gmail.com>
2022-07-04 06:58:16 +0300

    Add DOM handles to navigation.js file

---
+++ modules/navigation.js
@@ -0,0 +1,10 @@
+const bookList = document.getElementById('booklist');
+const dateDiv = document.getElementById('date');
+
+const bookContainer = document.getElementById('book-con');
+const addBookContainer = document.getElementById('add-book');
+const contactInformation = document.getElementById('cont-info');
+
+const list = document.getElementById('list');
+const addNew = document.getElementById('add-new');
+const contact = document.getElementById('contact');
